0e61efd01957a8357cb24b5f6f6ab044dad15db0
YU <yu1085@naver.com>
2022-09-24 07:36:24 +0900

    playground test

---
+++ playground.playground/Pages/TestOne.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+let title: String = "Hello, Test number 1"
+
+print(title)
---
+++ playground.playground/Pages/TestTwo.xcplaygroundpage/Contents.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+let title: String = "Hello, Test number"
+let num:Int = 2
+
+print("\(title) \(num)")
